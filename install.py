5ee863d2e83c8af4ff5a2d0c2dd97640bb6c5df6
Edith Forsaken <138196095+EdithForsaken@users.noreply.github.com>
2026-01-27 13:03:21 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,13 +1,4 @@
 import launch
 
-packages = [
-    "huggingface_hub>=0.20.0",
-    "requests"
-]
-
-for pkg in packages:
-    if not launch.is_installed(pkg.split(">=")[0]):
-        launch.run_pip(
-            f"install {pkg}",
-            pkg
-        )
+if not launch.is_installed("huggingface_hub"):
+    launch.run_pip("install huggingface_hub", "huggingface_hub")
